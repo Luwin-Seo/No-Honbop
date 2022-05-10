523eb8bdebb82c0dd55459fd2d25bd496bf283b0
Luwin-Seo <aksdnjs88@gmail.com>
2022-05-10 18:48:11 +0900

    로그인 오류 수정 - 에이젝스 필수 스크립트 추가

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
         return render_template('index.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
