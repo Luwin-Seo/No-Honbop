a4628aee71f9904198fc34f24e38f7537215dc73
Luwin-Seo <aksdnjs88@gmail.com>
2022-05-12 03:07:12 +0900

    요약카드들이 이제 검색을 지원합니다 두 칸중 하나라도 공란으로 내놓으면 검색되지 않습니다 아마도 추후에 공란 방지나 공란일경우 전체검색기능을 추가하면 될 듯 합니다

--- app.py
+++ app.py
@@ -35,6 +35,22 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+@app.route('/database', methods=['GET'])
+def database():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        user_info = db.users.find_one({"id": payload["id"]})
+
+        post_list = list(db.posts.find({}, {'_id': False}))
+        return jsonify({'posts': post_list})
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
 
 @app.route('/login')
 def login():
